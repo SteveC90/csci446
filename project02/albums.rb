616e05199f30676ed258b6293855a288e2b99916
Esteban Chavez <echavez@mymail.mines.edu>
2013-01-24 16:39:58 -0700

    form renders

--- project02/albums.rb
+++ project02/albums.rb
@@ -2,8 +2,28 @@ require 'rack'
 
 class HelloWorld
   def call(env)
-    [200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
+  	request = Rack::Request.new(env)
+  	case request.path
+  	when "/form"
+  		render_form(request)
+  	when "/list"
+  		render_list(request)
+  	else
+  		[404, {"Content-Type" => "text/plain"}, ["Nothing here!"]]
+  	end
   end
+
+  def render_form(request)
+  	response = Rack::Response.new
+  	File.open("form.html", "rb") { |form| response.write(form.read)}
+  	response.finish
+  end
+
+  def render_list(request)
+  	response = Rack::Response.new(request.path)d
+  	respone.finish
+  end
+
 end
 
 Rack::Handler::WEBrick.run HelloWorld.new, :Port => 8080
