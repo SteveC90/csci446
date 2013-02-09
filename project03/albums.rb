19f1356e1579cb790c9426e97248d71ae8915797
Esteban <esteban@ubuntu.ubuntu-domain>
2013-02-09 16:08:02 -0700

    Converted html files to erb files.

--- project03/albums.rb
+++ project03/albums.rb
@@ -5,18 +5,15 @@ class AlbumApp
   def call(env)
   	request = Rack::Request.new(env)
   	case request.path
-  	when "/form"
-  		render_form(request)
-  	when "/list"
-  		render_list(request)
-  	else
-  		[404, {"Content-Type" => "text/plain"}, ["Nothing here!"]]
+  	when "/form" then render_form(request)
+  	when "/list" then render_list(request)
+  	else [404, {"Content-Type" => "text/plain"}, ["Nothing here!"]]
   	end
   end
 
   def render_form(request)
   	response = Rack::Response.new
-  	File.open("form.html", "rb") { |form| response.write(form.read)}
+  	response.write(ERB.new(File.read("form.html.erb")).result(binding))
   	response.finish
   end
 
@@ -25,26 +22,7 @@ class AlbumApp
 	albums = File.readlines("top_100_albums.txt").each_with_index.map { |s,i| Album.new(s, i+1) }
 	albums.sort! { |a, b| a.send(request.params["order"].intern) <=> b.send(request.params["order"].intern) }
 
-	response.write(
-	"<html>
-	 <head>
-	 	<title>The List</title>
-	 </head>
-	 <body>
-	 	<h1>Rolling Stone's Top 100 Albums of All Time</h1>
-	 	<h3>Sorted by " + request.params["order"].capitalize+"</h3>
-	 	<table>")
-
-	 albums.each do |x|
-	 	response.write("<tr")
-	 	if x.rank==request.params["rank"].to_i
-	 		response.write(" style=\"color:orange;\"")
-	 	end
-	 	response.write("><td> #{x.rank.to_s} </td><td> #{x.name} </td><td> #{x.year} </td></tr>")
-	 end
-	
-	response.write("</table> </body> </html>")
-
+	response.write(ERB.new(File.read("list.html.erb")).result(binding))
 	response.finish
   end
 
