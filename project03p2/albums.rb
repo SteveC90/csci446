6151c8b3aadd819f3426230b94e17c9660f10309
Esteban <esteban@ubuntu.ubuntu-domain>
2013-02-09 20:27:58 -0700

    Completed

--- project03p2/albums.rb
+++ project03p2/albums.rb
@@ -5,18 +5,12 @@ require_relative 'album'
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/albums.sqlite3.db")
 set :port, 8080
 
-before do
-	@@NUM = Album.count
-end
-
 post "/list" do
-	albums = Album.all(:order => params[:order].intern.asc)
+	@albums = Album.all(:order => params[:order].intern.asc)
 	erb :list
 end
 
 get "/form" do
+	@num = Album.count
 	erb :form
 end
-
-
-
